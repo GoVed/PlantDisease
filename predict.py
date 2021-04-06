c385e8fa91155eef8f2173ceadc342659d82ec9f
Ved Suthar <34582568+GoVed@users.noreply.github.com>
2021-04-06 16:24:51 +0530

    Cherry Detection Model

--- predict.py
+++ predict.py
@@ -13,33 +13,47 @@ def predict_plant(image,skip_preprocess=False):
     if not skip_preprocess:
         image=PreProcess.preProcess(image)
     model = keras.models.load_model('plantDet3.h5')
-    plants = ['Apple','Cherry','Corn','Grape','Peach','PepperBell','Potato','Strawberry']    
-    pred = model.predict(np.array([image],dtype=np.float32))[0]
+    plants = ['Apple','Cherry','Corn','Grape','Peach','PepperBell','Potato','Strawberry'] 
     
-    if max(pred)>0.5:
+    pred = model.predict(np.array([image],dtype=np.float32))[0]
+    print('\n')
+    print('Plant class',pred) 
+    if max(pred)>0:
+        if max(pred)<50:
+            print('Less confidence')
         return predict_disease(image,np.argmax(pred),plants[np.argmax(pred)])
     else:
         return 'Failed to identify the plant'
     
 def get_disease_model(index):
-    modelName=['AppleDet.h5','','CornDet.h5','','','','','']
+    modelName=['AppleDet.h5','CherryDet.h5','CornDet.h5','','','','','']
     
     return keras.models.load_model(modelName[index])
     
 def get_disease_names(index):
-    disease_names=[['Scab','Black rot','Cedar apple rust','Healthy'],[],['Leaf spot','Common rust','Healthy','Leaf blight'],[],[],[],[],[]]
+    disease_names=[['Scab','Black rot','Cedar apple rust','Healthy'],['Healthy','MilDew'],['Leaf spot','Common rust','Healthy','Leaf blight'],[],[],[],[],[]]
     return disease_names[index]
     
 def predict_disease(image,index,plant_name):
     model = get_disease_model(index)
     disease_names = get_disease_names(index)
     pred = model.predict(np.array([image],dtype=np.float32))[0]
-    if max(pred)>0.5:
-        return plant_name+', '+disease_names[np.argmax(pred)]
+    print('Disease class',pred) 
+    if max(pred)>0:
+        if max(pred)<50:
+            print('Less confidence')
+        return 'Predicted:'+plant_name+', '+disease_names[np.argmax(pred)]
     else:
         return plant_name+', '+'Failed to identify the disease'
   
-# from PIL import Image
-# test1=np.asarray(Image.open('test1.jpg'))
-# test2=np.asarray(Image.open('test2.jpg'))
-# predict_plant(test1)
+from PIL import Image
+test1=np.asarray(Image.open('test1.jpg'))
+test2=np.asarray(Image.open('test2.jpg'))
+test3=np.asarray(Image.open('test3.jpg'))
+test4=np.asarray(Image.open('test4.jpg'))
+test5=np.asarray(Image.open('test5.jpg'))
+print(predict_plant(test1))
+print(predict_plant(test2))
+print(predict_plant(test3))
+print(predict_plant(test4))
+print(predict_plant(test5))
